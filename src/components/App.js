860dd4164492528ba4cfb95839d55020e14247b3
Kelly <kcocallaghan@gmail.com>
2021-03-16 12:24:48 -0500

    Add amplify w/ auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
 import '../styles/App.css';
 import Admin from './Admin';
 import React from 'react';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div className="App">
+      <h1>Auth set up!</h1>
       <Admin></Admin>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
